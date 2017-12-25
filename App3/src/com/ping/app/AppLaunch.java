824bd1cb5e510a3c15f49788c1426d3944b2b4a7
ILambuRI <lamburii@gmail.com>
2017-12-25 08:44:35 +0200

    First throw

---
+++ App3/src/com/ping/app/AppLaunch.java
@@ -0,0 +1,10 @@
+package com.ping.app;
+
+public class AppLaunch {
+
+	public static void main (String[] args) {
+		—heck—onnection connectCheck = new —heck—onnection();
+		connectCheck.play();
+	}
+
+}
